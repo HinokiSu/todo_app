d916d45912bfff193a5bbc38c14c8c810dce2824
Hinoki <yukihinoki@qq.com>
2021-12-22 10:43:49 +0800

    chore(home): remove this comp

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
+ 
   {
     path: '/',
-    name: 'home',
-    component: () => import('@/views/Home.vue'),
-    meta: {
-      requiresAuth: false,
-    },
-  },
-  {
-    path: '/login',
     name: 'login',
     component: () => import('@/views/Login.vue'),
     meta: {
